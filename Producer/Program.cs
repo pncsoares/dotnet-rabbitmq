da63af3d1dc648255872e789fa30c2c1ecfcae1f
Pedro Soares <pncsoares@gmail.com>
2022-06-15 08:40:18 +0100

    Add producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory
+{
+    // We are using AMQP pattern: amqp://{username}:{guest}@{hostname}:{port}
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+channel.QueueDeclare("demo-queue", true, false, false, null);
+
+var message = new
+{
+    Name = "Producer",
+    Message = "Hello World!"
+};
+
+var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+channel.BasicPublish(string.Empty, "demo-queue", null, body);
